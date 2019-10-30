906a7e220b0c3c02f39d2670237c79cb6b7efd38
sseen <ssnfee@gmail.com>
2019-10-31 00:07:53 +0800

    第一个布局

--- app/src/main/java/com/apple/droid/geoquiz/MainActivity.java
+++ app/src/main/java/com/apple/droid/geoquiz/MainActivity.java
@@ -3,12 +3,34 @@ package com.apple.droid.geoquiz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btTrue;
+    private Button btFalse;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btFalse =  findViewById(R.id.false_bt);
+        btTrue =  findViewById(R.id.true_bt);
+
+        btTrue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,R.string.true_toast,Toast.LENGTH_SHORT).show();
+            }
+        });
+        btFalse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,R.string.false_toast,Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
