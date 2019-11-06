84642dca481987847062c4873756ed21c8681921
sseen <ssnfee@gmail.com>
2019-11-06 15:21:55 +0800

    update

--- app/src/main/java/com/apple/droid/geoquiz/MainActivity.java
+++ app/src/main/java/com/apple/droid/geoquiz/MainActivity.java
@@ -5,20 +5,38 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button btTrue;
     private Button btFalse;
+    private Button btNext;
+    private TextView tvQuestion;
+
+    private Question[] mQuestionBank = new Question[] {
+            new Question(R.string.question_australia,true),
+            new Question(R.string.question_oceans,true),
+            new Question(R.string.question_mideast,true),
+            new Question(R.string.question_africa,true),
+            new Question(R.string.question_americas,true),
+            new Question(R.string.question_asia,true)
+    };
+
+    private int mCurrentIndex = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        tvQuestion = (TextView)findViewById(R.id.question_text_view);
+        updateQuestion();
+
         btFalse =  findViewById(R.id.false_bt);
         btTrue =  findViewById(R.id.true_bt);
+        btNext = findViewById(R.id.next_button);
 
         btTrue.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,5 +50,19 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this,R.string.false_toast,Toast.LENGTH_SHORT).show();
             }
         });
+        btNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+                updateQuestion();
+            }
+        });
+
     }
+
+    private void updateQuestion() {
+        int question = mQuestionBank[mCurrentIndex].getTextResId();
+        tvQuestion.setText(question);
+    }
+
 }
